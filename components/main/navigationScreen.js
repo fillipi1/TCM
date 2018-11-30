8122023c2197b961cbea76f34d8e3c41de94d2a4
fillipi <fillipi.rocha@yahoo.com>
2018-11-29 23:43:50 -0800

    re-cleaned navigation screen

--- components/main/navigationScreen.js
+++ components/main/navigationScreen.js
@@ -1,94 +1,145 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
+const joe = require('../../images/jre.png');
+const academy = require('../../images/academy.png');
+const forum = require('../../images/forum.png');
+const exam = require('../../images/exam.png');
+const files = require('../../images/files.png');
 
 class NavigationScreen extends React.Component {
 
-    render() {
-      return (
-            <View style={styles.container}>
-              <View style={styles.top}> 
-              </View>
-              <View style={styles.bottom}>
-                  <TouchableOpacity style={styles.bottomItem} onPress={() => Actions.login()} >
-                    <View style={[styles.bottomItemInner, { backgroundColor: '#A75252', borderColor: '#A75252' }]}>
-                      <Text style={[styles.text, { marginLeft: 22 }]}>
-                        Academy
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.bottomItem}>
-                    <View style={[styles.bottomItemInner, { backgroundColor: '#395176', borderColor: '#395176' }]}>
-                      <Text style={[styles.text, { marginLeft: 43 }]}>
-                        Forum
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.bottomItem}>
-                    <View style={[styles.bottomItemInner, { backgroundColor: '#5B9377', borderColor: '#5B9377' }]}>
-                      <Text style={[styles.text, { marginLeft: 17 }]}>
-                        Exam Prep
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.bottomItem}>
-                    <View style={[styles.bottomItemInner, { backgroundColor: '#D4D55D', borderColor: '#D4D55D' }]}>
-                      <Text style={[styles.text, { marginLeft: 10 }]}>
-                        Health Files
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-              </View>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.bottom}>
+          <View style={styles.top}>
+            <View style={{ alignItems: 'center', justifyContent: 'center', flexGrow: 1 }}>
+              <Image style={styles.logo} source={joe} />
+              <Text style={{ fontSize: 30, color: 'white' }}>
+                Joe Rogan
+              </Text>
             </View>
-);
-    }
-  }
+            <View style={styles.blueHeader}>
+              <TouchableOpacity style={styles.bottomItem2} onPress={() => Actions.login()} >
+                <View style={styles.bottomItemInner2}>
+                  <Image style={styles.logo} source={academy} />
+                  <Text style={styles.text}>
+                    Academy
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.bottomItem2}>
+                <View style={styles.bottomItemInner2}>
+                  <Image style={styles.logo} source={forum} />
+                  <Text style={styles.text}>
+                    Forum
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View> 
+          <TouchableOpacity style={{ width: 10 }} />
+          <TouchableOpacity style={styles.bottomItem} /> 
+          <TouchableOpacity style={styles.bottomItem}>
+            <View style={styles.bottomItemInner}>
+              <Image style={styles.logo} source={exam} />
+              <Text style={styles.text}>
+                Exam Prep
+              </Text>
+            </View>
+          </TouchableOpacity> 
+          <TouchableOpacity style={styles.bottomItem}>
+            <View style={styles.bottomItemInner}>
+              <Image style={styles.logo} source={forum} />
+              <Text style={styles.text}>
+                Health Files
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  } 
+}
   const styles = StyleSheet.create({
     container: {
       flex: 1, 
       backgroundColor: '#fff'
     },
     top: {
-      height: '10%',
-      alignItems: 'flex-start',
+      height: '25%',
       backgroundColor: '#3498db',
-      justifyContent: 'center'
+      width: '100%',
+      flexGrow: 1,
     },
-    profileImage: {
-      width: 100,
-      height: 100,
-      borderRadius: 80,
-      borderWidth: 3,
-      borderColor: '#fff',
-      backgroundColor: '#eee',
+    blueHeader: {
+      height: '25%',
+      backgroundColor: '#3498db',
+      width: '100%',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      display: 'flex',
+      
+    },
+    logo: {
+      width: 80,
+      height: 80,
+      borderRadius: 30,
+      alignItems: 'center',
+      justifyContent: 'center',
       },
       bottom: {
         height: '100%',
         backgroundColor: '#fff',
         flexDirection: 'row',
         flexWrap: 'wrap',
-        marginLeft: 10,
-        marginTop: 30,
+        display: 'flex',
       },
       bottomItem: {
         width: '50%',
         height: '30%',
-        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
       },
       bottomItemInner: {
         width: '95%',
-        height: '95%',
-        marginBottom: 20,
+        height: '85%',
         shadowOpacity: 1,
         shadowRadius: 3,
         shadowColor: 'black',
         shadowOffset: { width: 1, height: 1 },
-        borderRadius: 20,
-        borderWidth: 2,
+        borderRadius: 15,
+        backgroundColor: '#fff',
+        borderColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      bottomItem2: {
+        width: '50%',
+        height: '50%',
+        marginBottom: '45%',
+        marginTop: '5%',
+ 
+      },
+      bottomItemInner2: {
+        width: '95%',
+        height: '85%',
+        shadowOpacity: 1,
+        shadowRadius: 3,
+        shadowColor: 'black',
+        shadowOffset: { width: 1, height: 1 },
+        borderRadius: 15,
+        marginLeft: '2.3%',
+        backgroundColor: '#fff',
+        borderColor: '#fff',
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
       },
       text: {
-        color: 'white',
+        color: 'black',
         fontSize: 30,
         marginTop: 20
       }
